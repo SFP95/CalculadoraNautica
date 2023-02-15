6125b4272d27d368d60c6be72135ae04eab971b9
alumno <dam.sonia.fernandez@gmail.com>
2023-02-15 12:31:53 +0100

    creacion ejercicion area triangulo

--- src/main/java/AreaTriangulo/Cliente.java
+++ src/main/java/AreaTriangulo/Cliente.java
@@ -17,23 +17,24 @@ public class Cliente extends Conexion {
             output_Server = new DataOutputStream(skCliente.getOutputStream());
             output_cliente = new DataOutputStream(skCliente.getOutputStream());
 
-            //EJERCICIO AREA CIRCULO:
-            input_server = new DataInputStream(skCliente.getInputStream());
+            //EJERCICIO Triangulo:
+            System.out.println("\n-------------------------"+"CALCULAR BASE Y ALTURA DE UN TRIANGULO"+"\n-------------------------");
 
+            //Escribimos el mensaje de la pregunta de la base del triengulo al cliente desde el servidor
+            input_server = new DataInputStream(skCliente.getInputStream());
             String mensajeServer= input_server.readUTF();
             System.out.println(mensajeServer);
 
             String operacion= scan.next();
             output_cliente.writeUTF(operacion);
 
-            //System.out.println("CalculadoraNautica.Cliente a dicho "+operacion);
 
-            /*----- Lo que recibe el cliente de parte de servidor -----*/
+            /*----- La respuesta del calculo el cliente de realizado por servidor -----*/
 
-//            input_server= new DataInputStream(skCliente.getInputStream());
-//
-//            String respuesta= input_server.readUTF();
-//            System.out.println(respuesta);
+            input_server= new DataInputStream(skCliente.getInputStream());
+
+            String respuesta= input_server.readUTF();
+            System.out.println(respuesta);
 
         }catch (Exception e){
             //mensaje de error en caso de fallos en la conexión
--- src/main/java/AreaTriangulo/Servidor.java
+++ src/main/java/AreaTriangulo/Servidor.java
@@ -5,58 +5,49 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class Servidor extends Conexion {
-    String operacion;
 
     public Servidor() throws IOException {
         super("CalculadoraNautica.Servidor");
     }
     public void initServidor(){
         try {
-            skCliente =skServidor.accept(); // que queda a la espera de recibir la petición de conexión
-            System.out.println("- Conexión aceptada de : "+ skCliente.getInetAddress().getHostName());
-            System.out.println("------------\n");
+            // se queda a la espera de recibir la petición de conexión
+            skCliente =skServidor.accept();
+
+            //mensase de comprovación de conexción con el  servidor, mostrando la dirección IP y el host
+            System.out.println("- Conexión aceptada de : "+ skCliente.getInetAddress().getHostName()+"\n------------\n");
 
             //EJERCICIO:
-            output_cliente= new DataOutputStream(skCliente.getOutputStream());
-            output_cliente.writeUTF("¿Que necesita calcular?");
-            intput_cliente=new DataInputStream(skCliente.getInputStream());
+
+            //pedimos al cliente que nos de el primer dato en este caso : la base del triángulo
 
             //recogida de datos introducidos por cliente y mostrado en pantalla servidor
-            operacion = intput_cliente.readUTF();
-            //System.out.println("-He leido de cliente: "+operacion+"\n-----------");
 
-            //Metemos la recogida en una arrays de tipo string para poder recogerlo y
-            // separarlo en distintas variables : num 1, op,  y num2
-            String[] operacion_cliente=operacion.split(" ");
-           // System.out.println(Arrays.toString(operacion_cliente));
+            //para base:
+            output_cliente= new DataOutputStream(skCliente.getOutputStream());
+            output_cliente.writeUTF("Dime la base del triangulo");
+            intput_cliente=new DataInputStream(skCliente.getInputStream());
+            int base = intput_cliente.readInt();
 
+            //para altura:
 
-            int num1= Integer.parseInt(operacion_cliente[0]);
-            String op= operacion_cliente[1];
-            int num2= Integer.parseInt((operacion_cliente[2]));
+            output_cliente= new DataOutputStream(skCliente.getOutputStream());
+            output_cliente.writeUTF("Dime la altura del triangulo:");
+            intput_cliente=new DataInputStream(skCliente.getInputStream());
+            int altura = intput_cliente.readInt();
 
-            System.out.println("CalculadoraNautica.Cliente me ha dicho: "+num1+op+num2);
+            System.out.println("Cliente me ha dicho qye calcule: "+base+" y "+altura);
 
+            //operacion de la base por al altura
 
-            //OPERACIONES:
+             int operacion= base*altura;
 
-          /*  if (op.contains("-")){
-                int res = num1 - num2;
-                output_cliente.writeUTF("El resultado del servidor es: "+op);
-            }
-            if (op == "+"){
-                int res = num1 + num2;
-                output_cliente.writeUTF("El resultado del servidor es: "+res);
-            }
-            if (op == "*"){
-                int res = num1 * num2;
-                output_cliente.writeUTF("El resultado del servidor es: "+res);
-            }
-            if (op == "/"){
-                int res = num1 / num2;
-                output_cliente.writeUTF("El resultado del servidor es: "+res);
-            }
-*/
+             System.out.println("El resultado es: "+ operacion);
+
+           //mandamos la respuetsa a cliente
+
+            output_cliente.writeUTF("El aŕea del Triangulo es: "+ operacion);
+
         }catch (Exception e){
             //mensaje de error en caso de fallos en la conexión
             System.out.println("Errores encontrado en: " + e.getMessage());
