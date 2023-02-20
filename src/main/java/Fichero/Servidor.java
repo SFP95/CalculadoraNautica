bbfb70f66aa91ff81d4bf5ed405845a0521493eb
Nia <103027252+SFP95@users.noreply.github.com>
2023-02-20 12:44:58 +0100

    intento arreglo fichero

--- src/main/java/Fichero/Servidor.java
+++ src/main/java/Fichero/Servidor.java
@@ -19,30 +19,30 @@ public class Servidor extends Conexion {
 
             //recogida del fichero del cliente
             intput_cliente=new DataInputStream(skCliente.getInputStream());
-            String ficheroCliente= intput_cliente.readUTF();
-            System.out.println("---- FICHERO LEIDO:\n\t"+ficheroCliente);
+            ObjectInputStream fichIn= new ObjectInputStream(intput_cliente);
+            Object mensaje = fichIn.readObject();
+            System.out.println("---- FICHERO LEIDO:\n\t"+mensaje);
 
             //hacemos el resumen del fichero
             //transformamos el mensaje del cliente para podrer resumirlo
 
-            FileOutputStream fichero= new FileOutputStream(ficheroCliente);
-            ObjectOutputStream fichOut= new ObjectOutputStream(fichero);
+           // FileOutputStream fichero= new FileOutputStream(ficheroCliente);
             MessageDigest md= MessageDigest.getInstance("SHA");
-
+/*
             byte texto[]= fichero.toString().getBytes(); //resumimos
             md.update(texto); // actualizamos
             byte resumen[]=md.digest(); //calculamos el resument
             //escribimos
-            fichOut.writeObject(texto);
-            fichOut.writeObject(resumen);
+            fichIn.writeObject(texto);
+            fichIn.writeObject(resumen);
             //cerramos
-            fichOut.close();
+            fichIn.close();
             fichero.close();
 
             //mandamos el resumen al cliente
             output_cliente = new DataOutputStream(skCliente.getOutputStream());
             System.out.println("- Mandando resumen al servidor"); //error aqui
-            output_cliente.writeUTF(fichOut.toString());
+            output_cliente.writeUTF(fichOut.toString());*/
 
 
         }catch (Exception e){
