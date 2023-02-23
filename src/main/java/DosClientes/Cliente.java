45c5d80f5e144a8bb28f4ed9e3bce4d57b0b1b55
Nia <88581938+NiaD95@users.noreply.github.com>
2023-02-23 14:40:51 +0100

    23.02.23

--- src/main/java/DosClientes/Cliente.java
+++ src/main/java/DosClientes/Cliente.java
@@ -9,7 +9,7 @@ public class Cliente extends Conexion {
     public Scanner scan=new Scanner(System.in);
 
     public Cliente() throws IOException {
-        super("Clienta");
+        super("Cliente");
     }
     public  void  initCLiente(){
         try {
@@ -17,39 +17,12 @@ public class Cliente extends Conexion {
             output_Server = new DataOutputStream(skCliente.getOutputStream());
             output_cliente = new DataOutputStream(skCliente.getOutputStream());
 
-            //EJERCICIO Triangulo:
-            System.out.println("\n***************************************"+"\nCALCULAR BASE Y ALTURA DE UN TRIANGULO"+"\n***************************************");
+            //EJERCICIO 2 clientes
 
-            //Escribimos el mensaje de la pregunta de la base del triengulo al cliente desde el servidor
+            //2 hilos uno para escribir y otro para recibir (?)
+            Thread escribir = new Thread();
+            Thread leer = new Thread();
 
-            //imprimimos mensaje del servidor piciendo la base
-            input_server = new DataInputStream(skCliente.getInputStream());
-
-            String mensajeBase= input_server.readUTF();
-            System.out.println(mensajeBase);
-
-            //capturamos la respuesta en un variable y la mandamos al servidor
-            int base= scan.nextInt();
-            output_cliente.writeInt(base);
-
-            System.out.println("*******");
-
-            //imprimimos mensaje del servidor piciendo la altura
-            input_server = new DataInputStream(skCliente.getInputStream());
-
-            String mensajeAltura= input_server.readUTF();
-            System.out.println(mensajeAltura);
-
-            //capturamos la respuesta en un variable y la mandamos al servidor
-            int altura= scan.nextInt();
-            output_cliente.writeInt(altura);
-
-            /*----- La respuesta del calculo el cliente de realizado por servidor -----*/
-
-            input_server= new DataInputStream(skCliente.getInputStream());
-
-            String respuesta= input_server.readUTF();
-            System.out.println(respuesta);
 
         }catch (Exception e){
             //mensaje de error en caso de fallos en la conexión
--- src/main/java/DosClientes/Servidor.java
+++ src/main/java/DosClientes/Servidor.java
@@ -17,45 +17,15 @@ public class Servidor extends Conexion {
             //mensase de comprovación de conexción con el  servidor, mostrando la dirección IP y el host
             System.out.println("\t- Conexión aceptada de : "+ skCliente.getInetAddress().getHostName()+"\n\t------------\n");
 
-            //EJERCICIO:
-
-            //pedimos al cliente que nos de el primer dato en este caso : la base del triángulo
-
-            //recogida de datos introducidos por cliente y mostrado en pantalla servidor
-
-            //para base:
-            output_cliente= new DataOutputStream(skCliente.getOutputStream());
-            output_cliente.writeUTF("Dime la base del triangulo");
-
-            intput_cliente=new DataInputStream(skCliente.getInputStream());
-            int base = intput_cliente.readInt();
-            System.out.println("Cliente me ha dado la base "+ base);
-
-            System.out.println("******");
-
-            //para altura:
-
-            output_cliente= new DataOutputStream(skCliente.getOutputStream());
-            output_cliente.writeUTF("Dime la altura del triangulo:");
-
-            intput_cliente=new DataInputStream(skCliente.getInputStream());
-            int altura = intput_cliente.readInt();
-            System.out.println("Cliente me ha dado la altura "+ altura);
+            //EJERCICIO: dos clientes by Gonzalo
+            while (true) {
+                skCliente = skServidor.accept();
 
 
-            System.out.println("-------\n");
-            System.out.println("Cliente me ha dicho que calcule: "+base+" y "+altura);
 
-            //operacion de la base por al altura
-            System.out.println("******");
+            }
 
-            int operacion= base*altura;
 
-            System.out.println("El resultado es: "+ operacion);
-
-           //mandamos la respuetsa a cliente
-
-            output_cliente.writeUTF("El aŕea del Triangulo es: "+ operacion);
 
         }catch (Exception e){
             //mensaje de error en caso de fallos en la conexión
